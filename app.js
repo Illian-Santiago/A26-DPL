2155bec914a91db2c110443dda3045283f7783a6
Illian-Santiago <illianclase12345678@gmail.com>
2025-01-23 21:47:59 +0000

    Configuracion swagguer

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var favicon = require('serve-favicon');
 var path = require('path');
 var bicicletasAPIRouter = require('./routes/Api/bicicletas');
 var app = express();
+var swaggerJsdoc = require("swagger-jsdoc");
+var swaggerUi = require("swagger-ui-express");
 
 // 2. Configuraciones
 app.set('views', path.join(__dirname, 'views'));
@@ -40,6 +42,42 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// Opciones Swagger
+const options = {
+  definition: {
+    openapi: "3.1.0", // Utilizaremos OpenAPI en su versión 3.1.0
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options); // Analiza las opciones pasadas como parámetro y devuelve el objeto de especificación de Swagger convertido
+app.use(
+  "/api-docs",
+  // Proceso de configuración de Swagger UI
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
 
 // 6. Exportación del servidor
 module.exports = app;
